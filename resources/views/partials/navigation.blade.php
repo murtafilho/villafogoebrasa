d59f8761a8df583565726756c7f6818694287b2f
Roberto Luciano <murtafilho@gmail.com>
2025-12-07 14:37:17 -0300

    Add SVG hamburger icon with inline onclick for mobile menu

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -30,8 +30,10 @@
             </div>
 
             <!-- Mobile Menu Button -->
-            <button id="mobile-menu-btn" class="lg:hidden text-villa-cream p-2">
-                <i data-lucide="menu" class="w-6 h-6"></i>
+            <button id="mobile-menu-btn" class="lg:hidden text-villa-cream p-2" onclick="document.getElementById('mobile-menu').classList.toggle('hidden')">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
             </button>
         </div>
     </div>
