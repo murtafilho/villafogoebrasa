5921f149e9603a3032fb7fe670f3d06b652d090e
Roberto Luciano <murtafilho@gmail.com>
2025-12-18 09:44:54 -0300

    fix: remove processamento duplicado de guests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -201,17 +201,6 @@ class HomeController extends Controller
         $data = $request->validated();
         $data['status'] = Reservation::STATUS_PENDING;
 
-        // Processar número de convidados se vier como "2 pessoas"
-        if (isset($data['guests']) && is_string($data['guests'])) {
-            $guestsStr = trim($data['guests']);
-            // Extrair número do texto "2 pessoas" ou "7+ pessoas"
-            if (preg_match('/(\d+)/', $guestsStr, $matches)) {
-                $data['guests'] = (int) $matches[1];
-            } else {
-                $data['guests'] = 2; // Default
-            }
-        }
-
         Reservation::create($data);
 
         return response()->json([
